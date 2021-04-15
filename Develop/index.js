c09b32a3893bd65fa26f04491b7823046e85f3d9
Thomas Dunlap <ttdunlap33@gmail.com>
2021-04-15 14:01:50 -0600

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,13 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = ["What is the title?", "What is your GitHub username?", "What technologies were used to build this?", "What sections (in order) are going in the readme?"];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
